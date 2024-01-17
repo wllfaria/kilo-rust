9761f32f29c7c811e664b8bc9fbcbae975762c7a
Willians Faria <dev.willians.faria@gmail.com>
2024-01-17 18:53:33 -0300

    feat: editor ported up to step 25

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 use crossterm::{event::Event::Key, terminal};
-use errno::errno;
 use std::io::Result;
 
 mod input;
 mod keyboard;
+mod view;
 
 use input::*;
+use view::*;
 
 fn main() -> Result<()> {
     terminal::enable_raw_mode()?;
     loop {
+        if editor_refresh_screen().is_err() {
+            die("editor_refresh_screen");
+        };
         if editor_process_keypress() {
             break;
         }
     }
     terminal::disable_raw_mode()?;
     Ok(())
-}
-
-pub fn die<S: Into<String>>(message: S) {
-    let _ = terminal::disable_raw_mode();
-    eprintln!("{}: {}", message.into(), errno());
-    std::process::exit(1);
 }
