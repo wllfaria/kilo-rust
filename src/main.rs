ecaa2b0acbdd29243cd81024e817a29f5ab139de
Willians Faria <dev.willians.faria@gmail.com>
2024-01-17 16:17:04 -0300

    feat: editor ported up to step 21

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{event::Event::Key, terminal};
+use errno::errno;
+use std::io::Result;
+
+mod input;
+mod keyboard;
+
+use input::*;
+
+fn main() -> Result<()> {
+    terminal::enable_raw_mode()?;
+    loop {
+        if editor_process_keypress() {
+            break;
+        }
+    }
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
+
+pub fn die<S: Into<String>>(message: S) {
+    let _ = terminal::disable_raw_mode();
+    eprintln!("{}: {}", message.into(), errno());
+    std::process::exit(1);
 }
